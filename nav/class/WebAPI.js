150e9a26d7047569dd08bc9e3e8a16a24a13c035
costales <costales.marcos@gmail.com>
2020-09-29 19:48:13 +0200

    Jump NaN values

--- nav/class/WebAPI.js
+++ nav/class/WebAPI.js
@@ -385,12 +385,25 @@ WebAPI.prototype.set_radars = function() {
 		lat1 = lat2;
 		lng1 = lng2;
 	}
+
+	// Jump NaN values
+	var routeBoundaryPolygon_1_aux = "";
+	var routeBoundaryPolygon_2_aux = "";
+	routeBoundaryPolygon_1.split(" ").forEach(function (item) {
+		if (!isNaN(item))
+			routeBoundaryPolygon_1_aux = routeBoundaryPolygon_1_aux + item + " ";
+	});
+	routeBoundaryPolygon_2.split(" ").forEach(function (item) {
+		if (!isNaN(item))
+			routeBoundaryPolygon_2_aux = routeBoundaryPolygon_2_aux + item + " ";
+	});
+	routeBoundaryPolygon_2_aux = routeBoundaryPolygon_2_aux.trim();
+
 	// Search radars POI http://wiki.openstreetmap.org/wiki/Overpass_API
-	var poly_box = '(poly:\"' + routeBoundaryPolygon_1 + routeBoundaryPolygon_2 + '\");out;'
-	console.log('https://overpass-api.de/api/interpreter?data=node[highway=speed_camera]' + poly_box);
+	var poly_box = '(poly:\"' + routeBoundaryPolygon_1_aux + routeBoundaryPolygon_2_aux + '\");out;'
 	$.ajax({
 		url: 'https://overpass-api.de/api/interpreter?data=node[highway=speed_camera]' + poly_box,
-		timeout: 120000,
+		timeout: 90000,
 		dataType: 'xml',
 		success: this.OK_callback_set_radars.bind(this),
 		error: this.KO_callback_set_radars.bind(this)
